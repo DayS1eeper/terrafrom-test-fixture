1aa0c93b0faae7d20a245f8e2d61945fa9ad75c6
Vladyslav Mihun <mihunvlad@gmail.com>
2024-09-24 12:11:41 +0300

    Create main.tf

---
+++ conf1/main.tf
@@ -0,0 +1,16 @@
+variable "sleep_time" {
+  default = 1
+}
+variable "run_id" {
+  default = "run-abc"
+  sensitive = true
+}
+
+resource "null_resource" "wait2" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "sleep ${var.sleep_time}"
+  }
+}
